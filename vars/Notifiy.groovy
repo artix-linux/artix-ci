332730246b0166c7bf7ea6915496e07bf90df437
udeved <artoo@artixlinux.org>
2018-07-21 22:43:57 +0200

    add Notify & PostBuild steps

---
+++ vars/Notifiy.groovy
@@ -0,0 +1,13 @@
+#!/usr/bin/env groovy
+
+def call(String pkg) {
+
+    def subject = "FAILURE CI: ${env.JOB_NAME}/${pkg}"
+    def body = "<b>Failure</b><br><br>Project: ${env.JOB_NAME}/${pkg} <br>Build Number: ${env.BUILD_NUMBER} <br> URL of build: <a href=${env.BUILD_URL}></a>"
+
+    emailext {
+        body: body,
+        subject: subject,
+        to: "devs@artixlinux.org"
+    }
+}
---
+++ vars/PostBuild.groovy
@@ -0,0 +1,5 @@
+#!/usr/bin/env groovy
+
+def call(def pkg) {
+    pkg.postBuild()
+}
