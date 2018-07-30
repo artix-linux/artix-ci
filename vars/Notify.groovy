ef67367b4df3be881cf57fecbe6447653f00f5f7
artoo <artoo@artixlinux.org>
2018-07-30 18:40:37 +0200

    Notify: fix brackets

--- vars/Notify.groovy
+++ vars/Notify.groovy
@@ -5,9 +5,9 @@ def call(String pkg) {
     def subject = "FAILURE CI: ${env.JOB_NAME}/${pkg}"
     def body = "<b>Failure</b><br><br>Project: ${env.JOB_NAME}/${pkg} <br>Build Number: ${env.BUILD_NUMBER} <br> URL of build: <a href=${env.BUILD_URL}></a>"
 
-    emailext {
+    emailext (
         body: body,
         subject: subject,
         to: "devs@artixlinux.org"
-    }
+    )
 }
