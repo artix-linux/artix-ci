74ccefb567f7dee55474a290b52d0734c3d26f9b
artoo <artoo@artixlinux.org>
2018-08-10 23:54:56 +0200

    Notify: fix build_url

--- vars/Notify.groovy
+++ vars/Notify.groovy
@@ -3,7 +3,7 @@
 def call(String pkg) {
 
     def subject = "FAILURE CI: ${env.JOB_NAME}/${pkg}"
-    def body = "<b>Failure</b><br><br>Project: ${env.JOB_NAME}/${pkg} <br>Build Number: ${env.BUILD_NUMBER} <br> URL of build: <a href=${env.BUILD_URL}></a>"
+    def body = "<b>Failure</b><br><br>Project: ${JOB_NAME}/${pkg} <br>Build Number: ${BUILD_NUMBER} <br> URL of build: <a href=${BUILD_URL}></a>"
 
     emailext (
         body: body,
