f4a7c5da6cb46c84f47fa4aee6bbd72c0df595a5
artoo <artoo@artixlinux.org>
2019-01-03 20:07:07 +0100

    only attach log on build failure

--- vars/Notify.groovy
+++ vars/Notify.groovy
@@ -5,11 +5,12 @@ def call(def pkg, Boolean repoops, String msg, Boolean debug) {
     String body = ''
     String bodyInfo = ""
     String bodyAction = ''
-    string bodyRepo = "<p><strong>${msg}</strong></p>"
-    string bodyAuthor = "<p>authorName: ${pkg.authorInfo.name}</p><p>authorEmail: ${pkg.authorInfo.email}</p>"
+    String bodyRepo = "<p><strong>${msg}</strong></p>"
+    String bodyAuthor = "<p>authorName: ${pkg.authorInfo.name}</p><p>authorEmail: ${pkg.authorInfo.email}</p>"
     String bodyUrl = "<p><a href=${BUILD_URL}>${BUILD_URL}</a></p>"
 
     Boolean sendMail = false
+    Boolean isLogAttach = false
     String sendTo = 'artix-builds@artixlinux.org'
 
     if ( repoops ) {
@@ -25,13 +26,14 @@ def call(def pkg, Boolean repoops, String msg, Boolean debug) {
             }
         }
     } else {
-        bodyInfo = "<p>Job: ${JOB_NAME}</p>"
+        bodyInfo = "<p>Job: ${JOB_NAME}</p><p>Packages: ${pkg.pkgInfo.pkgfile}</p>"
         bodyAction = "<p>Build: ${pkg.repoPathGit}</p>"
 
         sendMail = true
 
         if ( msg == 'Failure' ) {
             sendTo = 'artix-build-failures@artixlinux.org'
+            isLogAttach = true
         }
     }
 
@@ -43,7 +45,7 @@ def call(def pkg, Boolean repoops, String msg, Boolean debug) {
                 body: body,
                 subject: subject,
                 to: sendTo,
-                attachLog: true
+                attachLog: isLogAttach
             )
         }
     }
